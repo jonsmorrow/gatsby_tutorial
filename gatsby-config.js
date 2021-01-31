cb6862c01b944db5730e13efbc7b16bc1525c527
Jon Morrow <jon@morrowmail.com>
2021-01-31 08:36:20 -0800

    Revert repo to initial state to begin tutorial section 4
    Revert "Merge pull request #14 from jonsmorrow/creating_nested_layout_components"
    
    This reverts commit 2c7e4196c63f7a828d13a9160be7f6c9ad5c680b, reversing
    changes made to ff5d9aa3bd06de7e00b9e23d455a0d65d58b2597.
    
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,8 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+
 module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
+  /* Your site config here */
+  plugins: [],
 }
