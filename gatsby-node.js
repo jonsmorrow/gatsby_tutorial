6d42ed2e65158870dedc0ce513d3a878cb297333
Jon Morrow <jon@morrowmail.com>
2021-02-01 16:07:52 -0800

    Creating slugs for pages - Update gatsby-node.js to create slugs for MarkdownRemark nodes
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,13 @@
-exports.onCreateNode = ({ node }) => {
-  console.log(`Node created of type "${node.internal.type}"`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
 }
